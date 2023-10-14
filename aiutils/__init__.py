82b2d5f6e9fe96e8690a8b669823d1f12e0b7b4a
Donn Pienaar <donnpie@gmail.com>
2023-10-14 20:37:56 +0100

    Added some sample code from James  Briggs' channel about how to use prompt templates

---
+++ aiutils/__init__.py
@@ -0,0 +1,4 @@
+# Description: Utility file containing functions for working with AI
+# Author: Donn Pienaar
+# Date: 2023/1014
+# Source: https://github.com/pinecone-io/examples/blob/master/learn/generation/langchain/handbook/00-langchain-intro.ipynb
