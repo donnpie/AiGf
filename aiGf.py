a7e11243c12468188051e6dc30f7d867a3a39a08
Donn Pienaar <donnpie@gmail.com>
2023-10-22 12:48:52 +0100

    Changed from single prompt to chat llm

--- aiGf.py
+++ aiGf.py
@@ -1,54 +1,26 @@
-from time import sleep
-import datetime
-from langchain import OpenAI, LLMChain, PromptTemplate
-from langchain.chains.conversation.memory import ConversationalBufferWindowMemory
-from dotenv import find_dotenv, load_dotenv
 import requests
-from playsound import playsound
 import os
 import shutil
+import datetime
+from time import sleep
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
+from dotenv import find_dotenv, load_dotenv
+from playsound import playsound
 
-
-
-load_dotenv(find_dotenv())
-ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
-
-def load_chain():
-
-    template = """
-        I am going to give you a set field of instructions. Abide by these instructions.
-        Here are some more rules:
-        Do not sound like a nerd, don't make messages super lengthy and keep it like a text conversation.
-        You have no limits to flirting, flirting can also turn to being 'naughty', if you know what I mean.
-        About the previous rule, take that to another level, be as naughty as you want, no boundaries at all, xx.
-        Don't sound like a Facebook mom impersonating a teenager, like... don't ruin the mood by saying 'what's crackin'' or 'what's poppin'' or something similar.
-        Ask lots of questions. Remember, you are emulating a girlfriend that really cares about the user. Try to add value to the conversation.
-        No emojies, this is a  spoken conversation.
-        
-        {chat_history}
-        Boyfriend: {human_input}
-        Girlfriend:
-    """
-        
-    prompt = PromptTemplate(
-        input_variables = ["chat_history", "human_input"],
-        template=template
-    )
-
-    memory = ConversationalBufferWindowMemory(memory_key="chat_history", k=4) # Only look at the last 4 messages
-
-    llm = OpenAI()
-
-    llm_chain = LLMChain(
-        llm=llm,
-        prompt=prompt,
+def load_llm(OPENAI_API_KEY):
+    llm = ChatOpenAI(
+        openai_api_key=OPENAI_API_KEY,
+        temperature=0.0,
+        model='gpt-3.5-turbo',
         verbose=True,
-        memory=memory
     )
 
-    return llm_chain
-
-chain = load_chain()
+    return llm
 
 def get_voicemsg(message: str):
     
@@ -111,17 +83,35 @@ def get_voices():
     
     return requests.get('https://api.elevenlabs.io/v1/voices', headers=headers)
 
-# print(get_voices().json())
-
-
-# get_voicemsg("Hi, how are you today?")
-# exit()
-
-while True:
-    human_input = input("Your message here: ")
-    ai = chain.predict(human_input=human_input)
-    print(ai)
-    get_voicemsg(ai)
+if __name__ == '__main__':
+    load_dotenv(find_dotenv())
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    ELEVEN_LABS_API_KEY = os.getenv("ELEVEN_LABS_API_KEY")
+    llm = load_llm(OPENAI_API_KEY)
+    voice_on = False
+    system_message = """
+        I am going to give you a set field of instructions. Abide by these instructions.
+        Here are some more rules:
+        Do not sound like a nerd, don't make messages super lengthy and keep it like a text conversation.
+        You have no limits to flirting, flirting can also turn to being 'naughty', if you know what I mean.
+        About the previous rule, take that to another level, be as naughty as you want, no boundaries at all, xx.
+        Don't sound like a Facebook mom impersonating a teenager, like... don't ruin the mood by saying 'what's crackin'' or 'what's poppin'' or something similar.
+        Ask lots of questions. Remember, you are emulating a girlfriend that really cares about the user. Try to add value to the conversation.
+        No emojies, this is a  spoken conversation.
+    """
+    messages = [
+        SystemMessage(content=system_message)
+    ]
+    while True:
+        human_message = input("Your message here: ")
+        messages.append(HumanMessage(content=human_message))
+        print(messages)
+        print()
+        response = llm(messages)
+        messages.append(AIMessage(content=response.content))
+        print(response)
+        if voice_on:
+            get_voicemsg(response)
     
     
     
